5e9fea69905bf8a36937360d095c74de16f6f976
Denis Beluchenko <dzibis@gmail.com>
2022-11-22 21:58:44 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class Main {
+    public static void main(String[] args) {
+        Queue<Person> queue = new ArrayDeque<>();
+        List<Person> people = generateClients();
+        for (Person person:people){
+            queue.offer(person);
+        }
+        while (!queue.isEmpty()){
+            Person person=queue.poll();
+            System.out.println("Посетитель "+person.getName()+" прокатился");
+            person.spentTickets();
+            if(person.getTickets()>0)
+            {
+                queue.offer(person);
+            }
+        }
+    }
+
+    public static List<Person> generateClients() {
+        List<Person> ans = new ArrayList<>();
+        ans.add(new Person("Denis", 3));
+        ans.add(new Person("Igor", 2));
+        ans.add(new Person("Tom", 1));
+        ans.add(new Person("Lisa", 1));
+        ans.add(new Person("Sem", 1));
+        return ans;
+    }
 }
+
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,21 @@
 public class Person {
+    protected String name;
+    protected int tickets;
+
+    public Person(String name, int tickets) {
+        this.name = name;
+        this.tickets = tickets;
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public  void spentTickets(){
+        tickets--;
+    }
 }
+
